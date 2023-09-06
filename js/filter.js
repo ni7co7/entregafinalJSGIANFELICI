98b0d8ded0823192c1080185bb66debd23455db9
ni7co7 <124326128+ni7co7@users.noreply.github.com>
2023-09-06 00:06:05 -0300

    Update filter.js
    filtro de imagenes dinamicas post plazo de entrega

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,67 @@
 
-const tipoDesarrolloSelect = document.getElementById('tipoDesarrollo'); //referencio el tipo de desarrollo
+function cargarValores() {
+    var selectA = document.getElementById('selectA');
+    var selectB = document.getElementById('selectB');
+    var combinacionValores = document.getElementById('combinacionValores');
+
+    var seleccionA = selectA.value;
+    var seleccionB = selectB.value;
+
+    // Crear una clave de combinación a partir de las selecciones
+    var combinacion = seleccionA + "-" + seleccionB;
+
+    // Realizar una solicitud HTTP para cargar el archivo JSON
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            // Obtener la ruta de la imagen basada en la combinación de selecciones
+            var imagenSrc = data.combinaciones[combinacion];
+
+            if (imagenSrc) {
+                // Crear un elemento de imagen
+                var imagenElement = document.createElement('img');
+                imagenElement.src = imagenSrc;
+                imagenElement.alt = "Imagen de la combinación";
+
+                // Mostrar la imagen en la página
+                combinacionValores.innerHTML = ''; // Limpiar contenido anterior
+                combinacionValores.appendChild(imagenElement);
+            } else {
+                // Mostrar un mensaje si no se encuentra la ruta
+                combinacionValores.textContent = "Combinación no válida";
+            }
+        })
+        .catch(error => {
+            console.error('Error al cargar el archivo JSON: ', error);
+        });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const tipoDesarrolloSelect = document.getElementById('tipoDesarrollo'); //referencio el tipo de desarrollo
 const cantidadHabitacionesSelect = document.getElementById('cantidadHabitaciones'); //referencio cant de habitaciones
 const resultadosDiv = document.getElementById('resultados'); //referencio el div donde se imprime el resultado
 const filtrarBtn = document.getElementById('filtrarBtn');//referencio el boton con la acci+on
@@ -63,7 +125,7 @@ filtrarBtn.addEventListener('click', async () => { //espero por evento click
     }
 });  
 
-
+ */
 
 
 
